b73f24454ad9c07b7a5eada01862f5d4d29c6300
Pavel Senin <seninp@gmail.com>
2014-10-14 16:48:00 +0200

    working on figure

--- RCode/figure_1.R
+++ RCode/figure_1.R
@@ -11,30 +11,19 @@ require(lubridate)
 #
 
 data=read.table("patternz.txt",as.is=T,sep=" ")
-dm1=as.data.frame(labels=paste()data[data[,1]=="pre",-1])
-dm2=as.data.frame(data[data[,1]=="post",])
-
-dm = rbind(pre_res[,c(2,c(7:34))], post_res[,c(2,c(7:34))])
-dm = cbind(utag=paste(dm[,1],row.names(dm),sep=""),dm)
-df <- melt(dm1,id.vars=c("V1"))
-p1=ggplot(dm1, aes(x=variable,y=value, group=utag)) + scale_y_log10() + geom_line(aes(colour = tag))
+dm1=as.data.frame(data[data[,1]=="pre",-1])
+dm1=dm1[1:4,]
+dm1$samples=paste("sample",c(1:length(dm1$V2)),sep="")
+df <- melt(dm1)
+p1=ggplot(df, aes(x=variable,y=value, group=samples, color=samples)) + scale_y_log10() + geom_line()
 
 
-dd = cbind(data.frame(utag="pre_mean",tag="pre_mean"),t(colMeans(pre_res[,c(7:34)])))
-dd = melt(dd,id.vars=c("utag","tag"))
-df <- rbind(df,dd)
+dm2=as.data.frame(data[data[,1]=="post",-1])
+dm2$samples=paste("sample",c(1:length(dm2$V2)),sep="")
+df2 <- melt(dm2)
+p2=ggplot(df2, aes(x=variable,y=value, group=samples, color=samples)) + scale_y_log10() + geom_line()
 
-dd = cbind(data.frame(utag="post_mean",tag="post_mean"),t(colMeans(post_res[,c(7:34)])))
-dd = melt(dd,id.vars=c("utag","tag"))
-df <- rbind(df,dd)
-
-
-# ggplot(df, aes(x=variable,y=value, group=utag)) + geom_path(aes(colour = tag))
-
-p1=ggplot(df[grep("^pre",df$tag),], aes(x=variable,y=value, group=utag)) + scale_y_log10() + geom_line(aes(colour = tag))
-p2=ggplot(df[grep("^post",df$tag),], aes(x=variable,y=value, group=utag)) + scale_y_log10() + geom_path(aes(colour = tag))
-
-print(arrangeGrob(p1, p2, ncol=1))
+print(arrangeGrob(p1, p2, ncol=2))
 
 write.table(pre_res,file="pre_portraits_monthly_google.txt", quote=F, col.names=T, row.names=F, sep="\t")
 write.table(post_res,file="post_portraits_monthly_google.txt", quote=F, col.names=T, row.names=F, sep="\t")
